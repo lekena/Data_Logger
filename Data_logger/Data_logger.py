0a51ff236173cc49d99c8c6007261860e7b8c5cc
Tlotlisang Lekena <repocookie@gmail.com>
2020-11-05 16:27:22 +0200

    Import the libraries

--- Data_logger/Data_logger.py
+++ Data_logger/Data_logger.py
@@ -1,2 +1,9 @@
-"""Main module."""
-print("First remote commit test ")
+import os
+import time
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+import adafruit_dht
+from Adafruit_IO import Client, Feed
